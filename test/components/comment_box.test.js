4e0a08005b98b9aa0c1ad9ba96262ec205b92c9e
Your Name <you@example.com>
2017-07-16 00:12:54 +0900

    TDD Refactor Test CommentBoxComponent. Using beforeEach for testing

--- test/components/comment_box.test.js
+++ test/components/comment_box.test.js
@@ -2,18 +2,21 @@ import { renderComponent, expect } from '../test_helper.js';
 import CommentBox from '../../src/components/commentBox';
 
 describe('CommentBox', () => {
-  it('has hte correct class', () => {
-    const component = renderComponent(CommentBox);
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox)
+  });
+
+  it('has the correct class', () => {
     expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
