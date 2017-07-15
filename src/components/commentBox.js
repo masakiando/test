c25903f731689d80ac61d9edddd0f32a7a93f495
Your Name <you@example.com>
2017-07-15 22:10:09 +0900

    I created a textarea and button on CommentBoxComponent, tested it and confirmed that the test was successful.

---
+++ src/components/commentBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div>
+        <textarea />
+        <button></button>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
