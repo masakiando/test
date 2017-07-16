80611b91b9f4cf17249c7d8c2cbf8ef26386f7fb
Your Name <you@example.com>
2017-07-17 01:11:03 +0900

    TDD Test CommentListComponent 1. 私はdescribeブロックを作成した。 2.その中にitブロックを２つ作成しテスト名を定義しました。

---
+++ test/components/commnent-list.js
@@ -0,0 +1,15 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/CommentList';
+
+describe('CommentList', () => {
+
+  it('shows an LI for each comment', () => {
+
+  });
+  　　
+
+  it('shows each comment that is provided', () => {
+
+  });
+
+});
